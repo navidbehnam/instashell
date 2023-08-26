3faba0e777ac733f2cba62869229bb3e9cc594d5
navidbehnam <112815940+navidbehnam@users.noreply.github.com>
2023-08-27 01:29:18 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#Author: github.com/thelinuxchoice
-#Instagram: @thelinuxchoice
+#Author: github.com/navidbehnam
+#Instagram: @behnazmohseni2
 trap 'echo exiting cleanly...; exit 1;' SIGINT SIGTSTP
 
 checkroot() {
